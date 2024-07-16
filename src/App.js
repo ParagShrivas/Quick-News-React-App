e08659e4255eeaded75f5b0f063a37c6e4f6e242
ParagShrivas <parag.shrivas24@gmail.com>
2024-07-17 02:28:21 +0530

    Fix Bug

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React, { Component } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 import News from './components/News'
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 export default class App extends Component {
   render() {
     return (
       <Router>
         <div>
-            <Navbar />
+          <Navbar />
           <Routes>
-            <Route exact path="/Quick-News-React-App" element={<News/>} />
+            <Route exact path="/Quick-News-React-App" element={<News />} />
+            <Route exact path="/" element={<News />} />
           </Routes>
         </div>
       </Router>
